af2ece79804b041957b93702be2996b085798602
MitchZas <mitchellzasa@gmail.com>
2025-04-29 09:03:34 -0400

    Grid Movement Implemented with arrow keys
    Need to set it up so it works with controller, WASD, and joystick

---
+++ Wing Slinger/Assets/Scripts/GridMovement.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using System.Collections;
+
+public class GridMovement : MonoBehaviour
+{
+    [SerializeField] private bool isRepeatedMovement;
+    [SerializeField] private float moveDuration = 0.1f;
+    [SerializeField] private float gridSize = 1f;
+
+
+    private bool isMoving = false;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (!isMoving)
+        {
+            System.Func<KeyCode, bool> inputFunction;
+            if (isRepeatedMovement)
+            {
+                inputFunction = Input.GetKey;
+            }
+            else
+            {
+                inputFunction = Input.GetKeyDown;
+            }
+
+            if (inputFunction(KeyCode.UpArrow))
+            {
+                StartCoroutine(Move(Vector2.up));
+            }
+            else if (inputFunction(KeyCode.DownArrow))
+            {
+                StartCoroutine(Move(Vector2.down));
+            }
+            else if (inputFunction(KeyCode.LeftArrow))
+            {
+                StartCoroutine(Move(Vector2.left));
+            }
+            else if (inputFunction(KeyCode.RightArrow))
+            {
+                StartCoroutine(Move(Vector2.right));
+            }
+        }
+   }
+
+    private IEnumerator Move(Vector2 direction)
+    {
+        isMoving = true;
+
+        Vector2 startPosition = transform.position;
+        Vector2 endPosition = startPosition + (direction * gridSize);
+
+        float elaspedTime = 0;
+        while (elaspedTime < moveDuration)
+        {
+            elaspedTime += Time.deltaTime;
+            float percent = elaspedTime / moveDuration;
+            transform.position = Vector2.Lerp(startPosition, endPosition, percent);
+            yield return null; 
+        }
+
+        transform.position = endPosition;
+        isMoving = false;
+    }
+}
